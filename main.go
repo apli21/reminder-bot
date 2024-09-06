0441b7c45e1dc80e9c83e9c296ccd10beeb9adce
apli21 <113980937+apli21@users.noreply.github.com>
2024-09-07 02:33:47 +0300

    Add files via upload

--- main.go
+++ main.go
@@ -30,7 +30,6 @@ func main() {
 	}
 
 	var users_data = make(map[int64]user_data)
-
 	bot.Debug = true
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
@@ -38,16 +37,12 @@ func main() {
 	u.Timeout = 60
 
 	var tmp record
-	var records []record
+	records := new([]record)
 	var tmp_time time.Time
 	updates := bot.GetUpdatesChan(u)
+	go remind_sender(records, bot)
+
 	for update := range updates {
-		for _, rec := range records {
-			if rec.timer < time.Now().Unix() {
-				rec.chat_id.Text = rec.reminder
-				bot.Send(rec.chat_id)
-			}
-		}
 		if update.Message == nil { //пустые обновления игнорируем
 			continue
 		}
@@ -69,18 +64,21 @@ func main() {
 				users_data[update.Message.Chat.ID] = user_data{2, users_data[update.Message.Chat.ID].chat_id, update.Message.Text, users_data[update.Message.Chat.ID].loc, 0}
 				//дальше, запрашиваем дату и время для напоминания
 				bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Now, date of reminder in format dd:mm:yyyy hh:mm:ss. Or you can enter in incorrect foemat and break it, if you wish"))
-			}
-			if users_data[update.Message.Chat.ID].status == 2 { //если пользователь на этапе отправки даты
-				tmp_time, _ = time.ParseInLocation("02:01:2006 15:04:05", update.Message.Text, users_data[update.Message.Chat.ID].loc)
-				if tmp_time.Year() == 0001 { //проверяем, правильно ли введена дата, и дата ли это вообще
-					bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Uh, you either wrote in incorrect format, or you didn't input date at all"))
-				} else { //если всё верно, добавляем новый таймер в записи
-					tmp.chat_id = tgbotapi.NewMessage(update.Message.Chat.ID, " ")
-					tmp.loc = users_data[update.Message.Chat.ID].loc
-					tmp.reminder = users_data[update.Message.Chat.ID].reminder
-					tmp.timer = tmp_time.Unix() - time.Now().Unix()
-					records = append(records, tmp)
-					users_data[update.Message.Chat.ID] = user_data{0, update.Message.Chat.ID, "", users_data[update.Message.Chat.ID].loc, 0}
+			} else {
+				if users_data[update.Message.Chat.ID].status == 2 { //если пользователь на этапе отправки даты
+					tmp_time, _ = time.ParseInLocation("02:01:2006 15:04:05", strings.TrimSpace(update.Message.Text), users_data[update.Message.Chat.ID].loc)
+					if tmp_time.Year() == 0001 { //проверяем, правильно ли введена дата, и дата ли это вообще
+						bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "Uh, you either wrote in incorrect format, or you didn't input date at all"))
+					} else { //если всё верно, добавляем новый таймер в записи
+						tmp.chat_id = tgbotapi.NewMessage(update.Message.Chat.ID, " ")
+						tmp.loc = users_data[update.Message.Chat.ID].loc
+						tmp.reminder = users_data[update.Message.Chat.ID].reminder
+						tmp.timer = tmp_time.Unix() - time.Now().Unix()
+						*records = append(*records, tmp)
+						delete(users_data, update.Message.Chat.ID)
+					}
+				} else { //в любом другом случае, пишем пользователю напоминание о том как пользоваться этой штукой
+					bot.Send(tgbotapi.NewMessage(update.Message.Chat.ID, "To use this cursed abomination, use /new_timer command"))
 				}
 			}
 		} else { //в любом другом случае, пишем пользователю напоминание о том как пользоваться этой штукой
@@ -89,7 +87,23 @@ func main() {
 	}
 }
 
-func new_timer(update tgbotapi.Update, msg tgbotapi.MessageConfig, u tgbotapi.UpdateConfig, updates tgbotapi.UpdatesChannel, bot *tgbotapi.BotAPI) (record, bool) {
+func remind_sender(records *[]record, bot *tgbotapi.BotAPI) {
+	ticker := time.NewTicker(time.Second)
+	for range ticker.C {
+		log.Println("ticker works")
+		log.Println(len(*records))
+		for i, rec := range *records {
+			if rec.timer < time.Now().Unix() {
+				rec.chat_id.Text = rec.reminder
+				bot.Send(rec.chat_id)
+				(*records)[i] = (*records)[len(*records)-1]
+				(*records) = (*records)[:len(*records)-1]
+			}
+		}
+	}
+}
+
+/*func new_timer(update tgbotapi.Update, msg tgbotapi.MessageConfig, u tgbotapi.UpdateConfig, updates tgbotapi.UpdatesChannel, bot *tgbotapi.BotAPI) (record, bool) {
 	loc, _ := time.LoadLocation("Local")
 	var cancel bool
 	cancel = false
@@ -148,4 +162,4 @@ func new_timer(update tgbotapi.Update, msg tgbotapi.MessageConfig, u tgbotapi.Up
 	tmp.chat_id = msg
 	tmp.loc = loc
 	return tmp, cancel
-}
+}*/
